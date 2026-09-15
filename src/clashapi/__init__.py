049dbd4666f7e625b1864d045cf8aecb43a2217b
myhades <61046290+myhades@users.noreply.github.com>
2026-09-15 14:37:19 +0800

    feat: initialize Clash controller API package

---
+++ src/clashapi/__init__.py
@@ -0,0 +1,25 @@
+"""Async client for Clash-compatible controller APIs."""
+
+from .capabilities import CapabilityReport, EndpointCapability
+from .client import ClashAPI
+from .exceptions import (
+    APIAuthError,
+    APIClientError,
+    APIConnectionError,
+    APITimeoutError,
+    ClashAPIError,
+)
+from .models import FetchResult, VersionInfo
+
+__all__ = (
+    "APITimeoutError",
+    "APIAuthError",
+    "APIClientError",
+    "APIConnectionError",
+    "ClashAPI",
+    "ClashAPIError",
+    "EndpointCapability",
+    "CapabilityReport",
+    "FetchResult",
+    "VersionInfo",
+)
